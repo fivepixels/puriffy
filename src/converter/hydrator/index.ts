ff96daea3c937574683efbc155531fee05ce0f77
fivepixels <hi@cattynip.dev>
2024-04-23 14:45:53 -0400

    feat: update the hydrator

--- src/converter/hydrator/index.ts
+++ src/converter/hydrator/index.ts
@@ -1,46 +1,19 @@
-import type { CompiledTag } from "@type/converter/compile";
-
-interface HydratingSettings<T = object> extends CompiledTag {
+interface HydratingSettings<T = object> {
+  baseString: string;
   hydrateTarget: T;
 }
 
-function hydrate<T = object>({
+function hydrate<T extends object>({
   baseString,
-  markers,
   hydrateTarget,
 }: HydratingSettings<T>): string {
-  let positionOffset = 0;
+  for (const currentHydrationId in hydrateTarget) {
+    const currentHydrationValue = String(hydrateTarget[currentHydrationId]);
 
-  for (const currentMarker of markers) {
-    if (!currentMarker.positions) {
-      throw Error("The positions should exist.");
-    }
-
-    const actualCurrentMarkerLength = currentMarker.hydrationId.length + 4;
-    const currentReplacement =
-      hydrateTarget[currentMarker.hydrationId as keyof T];
-
-    let currentReplacementLength = 0;
-
-    if (typeof currentReplacement === "string") {
-      currentReplacementLength = currentReplacement.length;
-    } else if (typeof currentReplacement === "number") {
-      currentReplacementLength = currentReplacement.toString().length;
-    } else {
-      throw Error("All hydration value should be either string or number.");
-    }
-
-    for (const currentPosition of currentMarker.positions) {
-      const actualCurrentPosition = currentPosition + positionOffset;
-      const startPart = baseString.substring(0, actualCurrentPosition);
-      const endPart = baseString.substring(
-        actualCurrentPosition + actualCurrentMarkerLength,
-      );
-
-      positionOffset += currentReplacementLength - actualCurrentMarkerLength;
-
-      baseString = startPart + currentReplacement + endPart;
-    }
+    baseString = baseString.replaceAll(
+      `#(${currentHydrationId})#`,
+      currentHydrationValue,
+    );
   }
 
   return baseString;
