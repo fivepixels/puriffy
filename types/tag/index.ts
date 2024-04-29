8a87b34747b461318dd8cbb3ac226ebd99c419d9
Seol SO <hi@cattynip.dev>
2024-04-29 11:26:00 -0400

    feat: change typees

--- types/tag/index.ts
+++ types/tag/index.ts
@@ -1,28 +1,47 @@
-import type { Style } from "../styles";
-import type { AllTags } from "./names";
-
 export type DirectlyConvertibleTypes = string | number;
 export type DirectlyInconvertibleTypes = Tag | Tag[];
 export type BaseTagChildren =
   | DirectlyConvertibleTypes
   | DirectlyInconvertibleTypes;
 
-export interface BaseTag {
-  tag: AllTags;
+export type AllTag = keyof HTMLElementTagNameMap;
+export type ScriptType = "src" | "action";
+
+export interface Tag {
+  tag: AllTag;
   id?: string;
-  class?: string;
-  styles?: Style;
+  style?: string;
   children?: BaseTagChildren;
-}
-
-export type AnchorTagTarget = "current_tab" | "new_tab";
-export interface AnchorTag extends BaseTag {
   href?: string;
-  target?: AnchorTagTarget;
-}
-
-export interface TextArea extends BaseTag {
+  src?: string;
+  alt?: string;
+  target?: string;
+  rel?: string;
+  property?: string;
   name?: string;
+  content?: string;
 }
 
-export type Tag = BaseTag | AnchorTag | TextArea;
+export interface Script {
+  type: ScriptType;
+  content: string;
+}
+
+export interface Head {
+  title: string;
+  description: string;
+  author: string;
+  keywords: string[];
+  lang: string;
+}
+
+export interface Body {
+  nav?: Tag[];
+  main: Tag[];
+  footer?: Tag[];
+}
+
+export interface Page {
+  head: Head;
+  body: Body;
+}
