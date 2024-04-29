b71fc3cafe0a6ee8a840acdd3d9e2b682b972518
Seol SO <hi@cattynip.dev>
2024-04-29 11:35:13 -0400

    fix: change the function names

--- test/core/compiler.test.ts
+++ test/core/compiler.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import compile from "@src/core/compiler";
+import { compileTag } from "@src/core/compiler/compileTag";
 
 import {
   simpleWithChildren,
@@ -9,21 +9,21 @@ import {
 
 describe("testing simple compilation", () => {
   test("testing tags generation", () => {
-    const compiledResult = compile(simpleWithPlain);
+    const compiledResult = compileTag(simpleWithPlain);
     expect(compiledResult).toStrictEqual(
       "<main><h1>Test the h1 tag</h1><p>Test the p tag</p><div>Test the div tag</div></main>",
     );
   });
 
   test("testing properties generation", () => {
-    const compiledResult = compile(simpleWithProperties);
+    const compiledResult = compileTag(simpleWithProperties);
     expect(compiledResult).toStrictEqual(
       `<main><a href="https://github.com/fivepixels/" target="new_tab">Test the a tag</a><textarea name="Test the textarea tag"></textarea></main>`,
     );
   });
 
   test("testing children generation", () => {
-    const compiledResult = compile(simpleWithChildren);
+    const compiledResult = compileTag(simpleWithChildren);
     expect(compiledResult).toStrictEqual(
       "<main><h1>Test the h1 tag</h1><p>Test the p tag</p><div><h2>Test children tags</h2><p>Test children tags</p></div></main>",
     );
