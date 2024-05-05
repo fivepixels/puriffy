69aeb75e959193f03f1bf84122c54055fbf01e0a
Seol SO <hi@cattynip.dev>
2024-05-04 20:25:49 -0400

    feat: change the file name of compile.ts

--- src/core/compile/index.ts
+++ src/core/compile/index.ts
@@ -1,18 +1,10 @@
 import { fancyLog } from "@src/utils/fancyLog";
+import build from "./builder";
 import check from "./checker";
 
 async function compile() {
-  // check
-  //
-  // find the directory /app
-  // find the file puriffy.config.ts
-  // find the directory /page & find the file tree
-  // check if all exported values are in the right shape
-  //
   // build
   //
-  // make a directory /puriffied(might be changed by the "outDir" value)
-  // make a file server.js, which will be eventually run by the computer
   // make a info.json, which will contain all pages' methods
   // make directories based on found the file tree
   // make the index.html in each folder inside of the directories, which will be the pages
@@ -25,9 +17,9 @@ async function compile() {
 
   fancyLog(checkingResult);
 
-  if (checkingResult.hasError) {
-    return;
-  }
+  await build();
 }
+
+compile();
 
 export default compile;
